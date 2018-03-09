4c3d288be9e62f6b9190a7722a97e0f542bae874
Sybren A. Stüvel <sybren@stuvel.eu>
2018-03-09 14:24:06 +0100

    Add mypy invocation to unit test
    This way it's easy to run both the tests and mypy.

---
+++ tests/test_mypy.py
@@ -0,0 +1,24 @@
+import pathlib
+import unittest
+
+import mypy.api
+
+import blender_asset_tracer
+
+
+class MypyRunnerTest(unittest.TestCase):
+    def test_run_mypy(self):
+        path = pathlib.Path(blender_asset_tracer.__file__).parent
+        result = mypy.api.run(['--incremental', str(path)])
+
+        stdout, stderr, status = result
+
+        messages = []
+        if stderr:
+            messages.append(stderr)
+        if stdout:
+            messages.append(stdout)
+        if status:
+            messages.append('Mypy failed with status %d' % status)
+        if messages:
+            self.fail('\n'.join(['Mypy errors:'] + messages))
