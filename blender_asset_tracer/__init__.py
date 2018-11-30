595c02a07d9b5fa8f63efd7911acfc40ff703ba8
Sybren A. Stüvel <sybren@stuvel.eu>
2018-11-30 13:59:33 +0100

    Bumped version to 0.6

--- blender_asset_tracer/__init__.py
+++ blender_asset_tracer/__init__.py
@@ -20,4 +20,4 @@
 
 # <pep8 compliant>
 
-__version__ = '0.5'
+__version__ = '0.6'
--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Sybren A. Stüvel'
 author = 'Sybren A. Stüvel'
 
 # The short X.Y version
-version = '0.5'
+version = '0.6'
 # The full version, including alpha/beta/rc tags
-release = '0.5'
+release = '0.6'
 
 
 # -- General configuration ---------------------------------------------------
