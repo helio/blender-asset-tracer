fd316c4594826fbfa74eb678a1cd4db6b54cb8e2
Sybren A. Stüvel <sybren@stuvel.eu>
2018-12-06 10:02:40 +0100

    Bumped version to 0.7

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Sybren A. Stüvel'
 author = 'Sybren A. Stüvel'
 
 # The short X.Y version
-version = '0.6.1'
+version = '0.7'
 # The full version, including alpha/beta/rc tags
-release = '0.6.1'
+release = '0.7'
 
 
 # -- General configuration ---------------------------------------------------
