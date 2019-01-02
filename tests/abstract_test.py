7df73d6c693e308406989e04e28da2e0d6b43423
Sybren A. Stüvel <sybren@stuvel.eu>
2019-01-02 15:38:34 +0100

    Configure test logging at import time (rather than at setup time)

--- tests/abstract_test.py
+++ tests/abstract_test.py
@@ -4,14 +4,14 @@ import unittest
 
 from blender_asset_tracer import blendfile
 
+logging.basicConfig(
+    format='%(asctime)-15s %(levelname)8s %(name)s %(message)s',
+    level=logging.INFO)
 
 class AbstractBlendFileTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.blendfiles = pathlib.Path(__file__).with_name('blendfiles')
-        logging.basicConfig(
-            format='%(asctime)-15s %(levelname)8s %(name)s %(message)s',
-            level=logging.INFO)
 
     def setUp(self):
         self.bf = None
