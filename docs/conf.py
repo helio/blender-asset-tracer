f0836417a4a936d96d392fbb64da11b1cd3b87f5
Sybren A. Stüvel <sybren@stuvel.eu>
2019-06-12 11:49:27 +0200

    Bumped version to 1.2-dev0

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Sybren A. Stüvel'
 author = 'Sybren A. Stüvel'
 
 # The short X.Y version
-version = '1.1.1'
+version = '1.2-dev0'
 # The full version, including alpha/beta/rc tags
-release = '1.1.1'
+release = '1.2-dev0'
 
 
 # -- General configuration ---------------------------------------------------
