4e51b82e4c9b6b297455858900ca798d39d58779
Sybren A. Stüvel <sybren@blender.org>
2021-07-22 12:03:12 +0200

    Bumped version to 1.5b0

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Sybren A. Stüvel'
 author = 'Sybren A. Stüvel'
 
 # The short X.Y version
-version = '1.4.1'
+version = '1.5b0'
 # The full version, including alpha/beta/rc tags
-release = '1.4.1'
+release = '1.5b0'
 
 
 # -- General configuration ---------------------------------------------------
