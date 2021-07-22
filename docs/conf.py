cd42417c370bdcf3acd8d81df8369cacffb5c830
Paul Golter <paul@blender.org>
2021-07-22 12:38:33 +0200

    Bumped version to 1.6b0

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Sybren A. Stüvel'
 author = 'Sybren A. Stüvel'
 
 # The short X.Y version
-version = '1.5'
+version = '1.6b0'
 # The full version, including alpha/beta/rc tags
-release = '1.5'
+release = '1.6b0'
 
 
 # -- General configuration ---------------------------------------------------
