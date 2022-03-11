fab20900801b77396107431eaf79ae4ef321eaec
Sybren A. Stüvel <sybren@blender.org>
2022-03-11 17:28:46 +0100

    Bump version to 1.12-dev0

--- blender_asset_tracer/__init__.py
+++ blender_asset_tracer/__init__.py
@@ -20,4 +20,4 @@
 
 # <pep8 compliant>
 
-__version__ = "1.11"
+__version__ = "1.12-dev0"
--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Sybren A. Stüvel'
 author = 'Sybren A. Stüvel'
 
 # The short X.Y version
-version = '1.11'
+version = '1.12-dev0'
 # The full version, including alpha/beta/rc tags
-release = '1.11'
+release = '1.12-dev0'
 
 
 # -- General configuration ---------------------------------------------------
